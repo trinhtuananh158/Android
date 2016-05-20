a8f0e2c6a1818722d47a7e3d85b44250f172ccf4
trinhtuananh158 <trinhtuananh158@github.com>
2016-05-20 18:02:01 +0700

    20/05

--- Music/app/src/main/java/com/tuananh/music/MainActivity.java
+++ Music/app/src/main/java/com/tuananh/music/MainActivity.java
@@ -1,12 +1,21 @@
 package com.tuananh.music;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,18 +32,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         btnStop.setVisibility(View.GONE);
-        Toast.makeText(this, Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/cogai.mp3",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, Environment.getExternalStorageDirectory()+"/Download/cogai.mp3",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isMyServiceRunning(MyService.class))
+        {
+            btnStop.setVisibility(View.VISIBLE);
+            btnPlay.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "Destroy",Toast.LENGTH_LONG).show();
+        stopService(new Intent(getBaseContext(),MyService.class));
+        super.onDestroy();
+
     }
 
     public void playMusic(View view){
         startService(new Intent(getBaseContext(),MyService.class));
         btnStop.setVisibility(View.VISIBLE);
         btnPlay.setVisibility(View.GONE);
+        /*MediaPlayer player=new MediaPlayer();
+        try {
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/cogai.mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player.setLooping(true); // Set looping
+        player.setVolume(100, 100);
+        try {
+            player.prepare();
+            player.start();
+            Toast.makeText(this, "Service started",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+        /*String path = Environment.getExternalStorageDirectory()+"/Download/cogai.mp3";
+        MediaPlayer player = new MediaPlayer();
+        File file = new File(path);
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            player.setDataSource(inputStream.getFD());
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            player.prepare();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player.start();*/
     }
 
     public void stopMusic(View view){
         stopService(new Intent(getBaseContext(),MyService.class));
         btnStop.setVisibility(View.GONE);
         btnPlay.setVisibility(View.VISIBLE);
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Music/app/src/main/java/com/tuananh/music/MyService.java
+++ Music/app/src/main/java/com/tuananh/music/MyService.java
@@ -2,11 +2,16 @@ package com.tuananh.music;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -25,6 +30,7 @@ public class MyService extends Service {
         super.onCreate();
         player=new MediaPlayer();
         try {
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
             player.setDataSource(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/cogai.mp3");
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,15 +44,47 @@ public class MyService extends Service {
         try {
             player.prepare();
             player.start();
-        } catch (IOException e) {
+            Toast.makeText(this, "Service started",Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return 1;
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+//        super.onDestroy();
         player.stop();
         player.release();
+        Toast.makeText(this, "Service destroyed",Toast.LENGTH_LONG).show();
     }
+
+    /*public void playSound(String soundPath){
+        MediaPlayer m = new MediaPlayer();
+
+        m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+
+        });
+
+        try {
+
+            AssetFileDescriptor descriptor = getBaseContext().getAssets().openFd(soundPath);
+            m.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(),
+                    descriptor.getLength());
+
+            descriptor.close();
+
+            m.prepare();
+            m.setVolume(100f, 100f);
+            m.setLooping(false);
+            m.start();
+
+        } catch (Exception e) {
+        }
+    }*/
 }
